89b3055fe3086bcdc5b4c262f79d9cb0cbc69f29
caiwuu <2252711582@qq.com>
2022-03-27 23:30:55 +0800

    优化

--- src/components/block.js
+++ src/components/block.js
@@ -2,7 +2,7 @@ import { Component } from '../core'
 export class Block extends Component {
   render() {
     return (
-      <div style='color:#666;background:#eee;padding:6px 20px;margin-bottom:10px;border-radius: 5px;'>
+      <div style='background:#eee;padding:6px 20px;margin-bottom:10px;border-radius: 5px;'>
         {this.props.children.length ? this.props.children : '一个块...'}
       </div>
     )
--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -3,7 +3,7 @@ import { Component } from '../core/model/index'
 export class Paragraph extends Component {
   render() {
     return (
-      <div style='color:#666;padding:6px 20px;'>
+      <div style='padding:6px 20px;'>
         {this.props.children.length ? this.props.children : '一个段落'}
       </div>
     )
