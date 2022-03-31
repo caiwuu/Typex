d700b7150b9bb3423c834c0477451a42a0fa9505
caiwuu <2252711582@qq.com>
2022-03-31 17:41:24 +0800

    1

--- src/componentTest.js
+++ src/componentTest.js
@@ -1,20 +1,40 @@
 import { createElement as h, mount, Component, createRef } from './core'
-
+import { throttle, HSLToRGB } from './core/share/utils'
 class Palette extends Component {
   constructor(props) {
     super(props)
-    this.state = { hue: 0, x: 0, y: 0 }
+    this.state = { hue: 0, x: 228, y: 0 }
   }
   render() {
     return (
       <div
         style={`background: linear-gradient(to top, rgba(0, 0, 0, 1), transparent), linear-gradient(to left, hsla(${this.state.hue}, 100%, 50%, 1), rgba(255, 255, 255, 1))`}
         onClick={this.handerClick}
+        onMousedown={this.handleMouseDown}
         class='palette'
       >
         <span style={`top:${this.state.y}px;left:${this.state.x}px;`} class='palette-picker'></span>
       </div>
     )
+  }
+  handleChange = throttle((e) => {
+    const { offsetX: x, offsetY: y } = e
+    this.setState({ x, y })
+  }, 30)
+
+  handleMouseDown = (e) => {
+    this.handleChange(e)
+    window.addEventListener('mousemove', this.handleChange)
+    window.addEventListener('mouseup', this.handleMouseUp)
+  }
+
+  handleMouseUp = () => {
+    this.unbindEventListeners()
+  }
+
+  unbindEventListeners() {
+    window.removeEventListener('mousemove', this.handleChange)
+    window.removeEventListener('mouseup', this.handleMouseUp)
   }
   setHue(hue) {
     this.setState({ hue: hue })
@@ -27,20 +47,71 @@ class Palette extends Component {
 class Hue extends Component {
   constructor(props) {
     super(props)
-    this.state = { y: 0 }
+    this.state = { x: 0, x2: 0 }
   }
   render() {
     return (
-      <div onClick={this.handerClick} class='hue'>
-        <span style={`top:${this.state.y}px`} class='hue-picker'></span>
+      <div class='picker'>
+        <div class='left'>
+          <div onMousedown={this.handleHueMouseDown} class='hue'>
+            <span style={`left:${this.state.x}px`} class='hue-picker'></span>
+          </div>
+          <div
+            onMousedown={this.handleTransparencyMouseDown}
+            class='transparency'
+            style='margin-top:4px'
+          >
+            <div
+              class='transparency-picker-bg'
+              style={`background: linear-gradient(to right, #FF000000 0%, #FF0000FF`}
+            ></div>
+            <div style='position:absolute;top:0'>
+              <span style={`left:${this.state.x2}px;`} class='transparency-picker'></span>
+            </div>
+          </div>
+        </div>
+        <div class='right'>
+          <div class='color-block'></div>
+        </div>
       </div>
     )
   }
-  handerClick = (e) => {
-    const { offsetY: y } = e
-    const hue = (y * 360) / 200
-    this.setState({ y: y })
+  // hue
+  handleHueChange = throttle((e) => {
+    const { offsetX: x } = e
+    const hue = (x * 360) / 200
+    const [R, G, B] = HSLToRGB(hue, 100, 50)
+    console.log(R, G, B)
+    this.setState({ x: x })
     this.props.paletteRef.current.setHue(hue)
+  }, 30)
+
+  handleHueMouseDown = (e) => {
+    this.handleHueChange(e)
+    window.addEventListener('mousemove', this.handleHueChange)
+    window.addEventListener('mouseup', this.handleMouseUp)
+  }
+
+  // Transparency
+  handleTransparencyChange = throttle((e) => {
+    const { offsetX: x2 } = e
+    this.setState({ x2: x2 })
+  }, 30)
+
+  handleTransparencyMouseDown = (e) => {
+    this.handleTransparencyChange(e)
+    window.addEventListener('mousemove', this.handleTransparencyChange)
+    window.addEventListener('mouseup', this.handleMouseUp)
+  }
+  // pub
+  handleMouseUp = () => {
+    this.unbindEventListeners()
+  }
+
+  unbindEventListeners() {
+    window.removeEventListener('mousemove', this.handleHueChange)
+    window.removeEventListener('mousemove', this.handleTransparencyChange)
+    window.removeEventListener('mouseup', this.handleMouseUp)
   }
 }
 class ColorPick extends Component {
@@ -50,7 +121,7 @@ class ColorPick extends Component {
   }
   render() {
     return (
-      <div>
+      <div style='font-size:0;width:228px;'>
         <Palette ref={this.paletteRef}></Palette>
         <Hue paletteRef={this.paletteRef}></Hue>
       </div>
--- src/core/share/utils.js
+++ src/core/share/utils.js
@@ -162,3 +162,57 @@ export function isPlainObject(obj) {
 export function isRegExp(v) {
   return _toString.call(v) === '[object RegExp]'
 }
+
+export function throttle(fn, wait) {
+  let inThrottle, lastFn, lastTime
+  return function () {
+    const context = this,
+      args = arguments
+    if (!inThrottle) {
+      fn.apply(context, args)
+      lastTime = Date.now()
+      inThrottle = true
+    } else {
+      clearTimeout(lastFn)
+      lastFn = setTimeout(function () {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args)
+          lastTime = Date.now()
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0))
+    }
+  }
+}
+export function debounce(fn, ms = 0) {
+  let timeoutId
+  return function (...args) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn.apply(this, args), ms)
+  }
+}
+
+export function HSLToRGB(h, s, l) {
+  s /= 100
+  l /= 100
+  const k = (n) => (n + h / 30) % 12
+  const a = s * Math.min(l, 1 - l)
+  const f = (n) => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)))
+  return [255 * f(0), 255 * f(8), 255 * f(4)].map((ele) => round(ele))
+}
+export function round(n, decimals = 0) {
+  return Number(`${Math.round(`${n}e${decimals}`)}e-${decimals}`)
+}
+
+export function RGBToHSL(r, g, b) {
+  r /= 255
+  g /= 255
+  b /= 255
+  const l = Math.max(r, g, b)
+  const s = l - Math.min(r, g, b)
+  const h = s ? (l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s) : 0
+  return [
+    60 * h < 0 ? 60 * h + 360 : 60 * h,
+    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+    (100 * (2 * l - s)) / 2,
+  ].map((ele) => round(ele))
+}
