f9431361f29650803e3ed783e5229d8016f36cb1
caiwuu <2252711582@qq.com>
2024-04-23 18:02:59 +0800

    1

--- packages/@typex-core/Typex.js
+++ packages/@typex-core/Typex.js
@@ -5,7 +5,7 @@ import initCore from './initCore'
 const abstractMethod = ['mount']
 export default class Typex {
   contentRef = createRef()
-  renderContent() {
+  renderContent () {
     return (
       <div id='editor-content' style='position:relative' ref={this.contentRef}>
         {this.renderPath()}
@@ -15,26 +15,26 @@ export default class Typex {
   constructor(options) {
     classCheck(new.target, Typex, abstractMethod)
     this.$path = options.path
-    options.path._editor = this
+    this.$path.$editor = this
     initCore({
       editor: this,
       formats: options.formats,
       plugins: options.plugins,
     })
   }
-  renderPath() {
+  renderPath () {
     return this.formater.render({ children: [this.$path] })
   }
-  on(eventName, fn) {
+  on (eventName, fn) {
     this.$eventBus.on(eventName, fn)
   }
-  emit(eventName, args) {
+  emit (eventName, args) {
     this.$eventBus.emit(eventName, args)
   }
-  focus(range) {
+  focus (range) {
     this.$eventBus.emit('focus', range)
   }
-  destroy() {
+  destroy () {
     this.$eventBus.emit('destroy')
   }
 
@@ -44,10 +44,10 @@ export default class Typex {
    * @returns {path}  
    * @memberof Typex
    */
-  queryPath(v) {
+  queryPath (v) {
     return queryPath(v, this.$path)
   }
-  
+
   /**
    * @description 查询两个path的共同path
    * @param {vdom|elm|position|path} v1
@@ -55,7 +55,7 @@ export default class Typex {
    * @returns {path}  
    * @memberof Typex
    */
-  queryCommonPath(v1, v2) {
+  queryCommonPath (v1, v2) {
     const path1 = this.queryPath(v1)
     const path2 = this.queryPath(v2)
     return path1.queryCommonPath(path2)
